6741e655ba4feac8efeaa104591b7b7ea0e56b10
Bazhanel <35490984+Bazhanel@users.noreply.github.com>
2018-10-11 15:52:39 +0300

    test spring boot in sonar (#14)
    * test spring boot in sonar
    
    * test spring boot in sonar

--- src/main/java/org/danit/energym3/Energym3Application.java
+++ src/main/java/org/danit/energym3/Energym3Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Energym3Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Energym3Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Energym3Application.class, args);
+  }
 }
