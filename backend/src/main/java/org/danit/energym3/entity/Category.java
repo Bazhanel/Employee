4613d2c0aa32d3c728568d80d5f9b7ff28e80e4e
bazhanel <bazhanel@gmail.com>
2018-11-08 19:02:31 +0200

    rename class Organization to Company

--- backend/src/main/java/org/danit/energym3/entity/Category.java
+++ backend/src/main/java/org/danit/energym3/entity/Category.java
@@ -2,9 +2,11 @@ package org.danit.energym3.entity;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.Column ;
+import javax.persistence.Entity ;
+
+
+import javax.persistence.GeneratedValue ;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
